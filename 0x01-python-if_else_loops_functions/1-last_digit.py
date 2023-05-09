323693dba3bec5a6b35e8034e9bf9b4b13be8af2
BraveScorpion <naction923@gmail.com>
2023-05-09 03:46:33 -0700

    negative bug fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
+last = number % 10
 
 if last == 0:
-	adjective = '0'
+    adjective = '0'
 elif last > 5:
-	adjective = 'greater than 5'
+    adjective = 'greater than 5'
 else:
 	adjective = 'less than 6 and not 0'
-print("last digit of {} is {} and is {}".format(number, last, adjective))
+print("Last digit of {} is {} and is {}".format(number, last, adjective))
