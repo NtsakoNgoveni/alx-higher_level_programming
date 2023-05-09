6d809de02e9394584e31aed3dc43523266647962
BraveScorpion <naction923@gmail.com>
2023-05-09 16:45:01 -0700

    9 fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if isinstance(number, int):
-        return number % 10
+    if number < 0:
+        num = (number * -1) % 10
+    else:
+        num = number % 10
+    print('{}'.format(num), end='')
+    return num
