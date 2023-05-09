f74aaf310454902ffbd8cd20b1b1292daa512dbe
BraveScorpion <naction923@gmail.com>
2023-05-09 16:00:36 -0700

    format specifier

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for k in range(0, 10):
-        if i == 9 and k == 9:
-            print('{}{}'.format(i, k))
-        else:
-            print('{}{}'.format(i, k), end=', ')
+for i in range(0, 100):
+    if i == 99:
+        print('{:02}'.format(i))
+    else:
+        print('{:02}'.format(i), end = ', ')
