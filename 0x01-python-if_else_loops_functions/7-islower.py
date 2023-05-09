abc23e19514dc3f2046d819388d8a6a10138ff9c
BraveScorpion <naction923@gmail.com>
2023-05-09 16:12:31 -0700

    bug fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 def is_lower(c):
     if c == '':
         return False
-    if ord(c) in range(97, 123):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
