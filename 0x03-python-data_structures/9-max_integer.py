dac5ed0a801fc3fcf117db1afccfd13198abcec3
BraveScorpion <naction923@gmail.com>
2023-05-14 08:14:27 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    num_max = my_list[0]
+    for i in my_list:
+        if i > num_max:
+            num_max = i
+    return num_max
