72fe5012380bda59ccece669071061bc8c3a0eda
BraveScorpion <naction923@gmail.com>
2023-05-29 07:24:51 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     truthy = True
     try:
         print('{:d}'.format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         truthy = False
     return (truthy)
