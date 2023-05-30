021da227f22ba9ab15c5656f3016beada362cc47
BraveScorpion <naction923@gmail.com>
2023-05-30 03:09:20 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
         size (int): size of the square instance
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
