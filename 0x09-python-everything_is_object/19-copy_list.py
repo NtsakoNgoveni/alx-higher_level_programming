419f1bee670d33298e88c08f1a960f68e66e8cc4
BraveScorpion <naction923@gmail.com>
2023-06-06 04:09:15 -0700

    19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-"""Defines a function that create list copy"""
-
-
 def copy_list(ls):
-    """ Create copy of list
-    Args:
-        ls (list): List to create a copy of
-    Return: Copy of list
-    """
     return ls[:]
