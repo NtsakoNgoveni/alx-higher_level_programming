54afa2ba16ce67ca60c6d81ccf3627bb79d23aee
BraveScorpion <naction923@gmail.com>
2023-06-12 09:42:05 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defien a function check if isnstance"""
+
+
+def is_same_class(obj, a_class):
+    """Return true if obj is instance of a_class"""
+    return isinstance(obj, a_class)
