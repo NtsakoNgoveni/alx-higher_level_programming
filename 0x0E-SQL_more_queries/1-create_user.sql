1b3bf5ce43294879d54ce7851d03762ca039a22b
BraveScorpion <naction923@gmail.com>
2023-07-19 12:02:23 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- create user and set password
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
