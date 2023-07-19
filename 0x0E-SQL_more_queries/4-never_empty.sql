cb24de329f28e1f1813bf5ac230f142c07e1aab0
BraveScorpion <naction923@gmail.com>
2023-07-19 11:47:31 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
-- creates table with specific description where id defaults to 1
+-- creates table with specific description
 CREATE TABLE IF NOT EXISTS id_not_null (
   id INT DEFAULT 1,
   name VARCHAR(256)
