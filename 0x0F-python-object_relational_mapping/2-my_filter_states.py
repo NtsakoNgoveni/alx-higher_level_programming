895f1070e55d43f174b1098440dd70c8f80569f3
BraveScorpion <naction923@gmail.com>
2023-08-20 02:52:44 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""Prints records for a given state"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states\
+                    WHERE LIKE BINARY {}\
+                    ORDER BY id ASC".format(sys.argv[4]))
+    res = cursor.fetchall()
+
+    for rec in res:
+        print(rec)
+
+    cursor.close()
+    db.close()
