33ce040bfb992eea11234586685c9f2109cc3907
BraveScorpion <naction923@gmail.com>
2023-08-20 01:06:07 -0700

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""Lists a states from a provided databse"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+    cursor.excute("SELECT * FROM states ORDER BY is ASC")
+    results = cursor.fetchall()
+
+    for record in results:
+        print(record)
+
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Solutions to ALX Object relational mapping
